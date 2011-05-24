2d2612037890d0564711c6e5220f5c0627bd9740
phickey <hickeypr@gmail.com>
2011-05-24 15:25:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wvc::Application.config.secret_token = '2b7a43c9a384b5e9e2ae0707321a1391b01f243eb98985cef73471ad1f21c308193609d2d4703b74f0b8f23018f7d2db54086ec471903f7c4a8149f4cbaa3712'
